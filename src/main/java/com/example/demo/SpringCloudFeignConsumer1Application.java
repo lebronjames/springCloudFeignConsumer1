280fc4bfc52704950a1d7f0b496b61adbf38a406
lebronjames <798258843@qq.com>
2017-10-31 11:18:36 +0800

    20171031

---
+++ src/main/java/com/example/demo/SpringCloudFeignConsumer1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFeignConsumer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFeignConsumer1Application.class, args);
+	}
+}
